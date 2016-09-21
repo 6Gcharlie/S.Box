7f7777c6435a24e6765ddc5a49914679d8686d93
6Gcharlie <6Gcharlie@LAPTOP-P9UPLQEC>
2016-09-21 12:20:54 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace S.Box
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Black;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.ForeColor = System.Drawing.Color.Chartreuse;
+            this.textBox1.Location = new System.Drawing.Point(106, 9);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Black;
+            this.button1.Font = new System.Drawing.Font("Impact", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Chartreuse;
+            this.button1.Location = new System.Drawing.Point(106, 34);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Click for answer";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Visible = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Chartreuse;
+            this.label1.Location = new System.Drawing.Point(103, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Thinking...";
+            this.label1.Visible = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.ErrorImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.ErrorImage")));
+            this.pictureBox1.Image = global::S.Box.Properties.Resources.IMG_3064;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 9);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(82, 82);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Britannic Bold", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Chartreuse;
+            this.label2.Location = new System.Drawing.Point(1, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(515, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Remember: only EVER use caps or symbols if specifically asked to in a code, other" +
+    "wise dont use them at all";
+            this.label2.Visible = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.Black;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox2.ForeColor = System.Drawing.Color.Chartreuse;
+            this.textBox2.Location = new System.Drawing.Point(212, 9);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(143, 20);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.Visible = false;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Black;
+            this.button2.ForeColor = System.Drawing.Color.Chartreuse;
+            this.button2.Location = new System.Drawing.Point(212, 34);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(143, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Going somewhere?";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Black;
+            this.button3.ForeColor = System.Drawing.Color.Chartreuse;
+            this.button3.Location = new System.Drawing.Point(100, 50);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(76, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Get started";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Visible = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Chartreuse;
+            this.label3.Location = new System.Drawing.Point(100, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(101, 25);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Welcome!";
+            this.label3.Visible = false;
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.Chartreuse;
+            this.label4.Location = new System.Drawing.Point(100, 34);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(134, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "and thanks for using S.Box";
+            this.label4.Visible = false;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.Black;
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox3.ForeColor = System.Drawing.Color.Chartreuse;
+            this.textBox3.Location = new System.Drawing.Point(247, 35);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(115, 20);
+            this.textBox3.TabIndex = 10;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Black;
+            this.button4.ForeColor = System.Drawing.Color.Chartreuse;
+            this.button4.Location = new System.Drawing.Point(182, 50);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(68, 23);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "skip guide";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Visible = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Impact", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Chartreuse;
+            this.label5.Location = new System.Drawing.Point(144, 36);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(97, 18);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Enter Username";
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Black;
+            this.button5.ForeColor = System.Drawing.Color.Chartreuse;
+            this.button5.Location = new System.Drawing.Point(247, 60);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 13;
+            this.button5.Text = "Begin!";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.Color.Chartreuse;
+            this.label6.Location = new System.Drawing.Point(213, 13);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Visible = false;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.ForeColor = System.Drawing.Color.Chartreuse;
+            this.label7.Location = new System.Drawing.Point(472, 9);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "V0.02";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(519, 111);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "S.Box";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace S.Box
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string theCode = textBox1.Text;
+
+            string theUsername = textBox3.Text;
+
+            if (theCode == "S.Box_protocol_B14")
+            {
+                label1.Text = "Control_interface_over-ride = enter: help";
+
+                button2.Visible = true;
+
+                textBox2.Visible = true;
+
+                button1.Enabled = false;
+
+                textBox1.Enabled = false;
+
+                label6.Visible = false;
+
+                button2.Text = "challenge";
+            }
+            
+
+if (theCode == "hello")
+{
+    label1.Text = "Code Valid";
+
+    label6.Text = "Why hello there " + (theUsername) + ". pleased to meet you!";
+}
+
+if (theCode == "a7jsb5")
+{
+    label1.Text = "Code Valid";
+
+    label6.Text = "S.Box is running at its full.capacity";
+}
+             
+if (theCode == "jwn6ma")
+{
+    label1.Text = "Code Valid";
+
+    label6.Text = "try typing:" + Environment.NewLine + Environment.NewLine + "S.Box_protocol_B14" + Environment.NewLine + Environment.NewLine + "into the text box...";
+}
+
+if (theCode == "okay")
+{
+    label1.Text = "Code Valid";
+
+    label6.Text = "okay what?";
+}
+
+if (theCode == "what is the code")
+{
+    label1.Text = "Code Valid";
+
+    label6.Text = "HAH! Did you think that it was going to be that easy to get a code!?" + Environment.NewLine + "To get a code, the Admin must first GIVE you one!" + Environment.NewLine + "...or you could find one...";
+}
+
+if (theCode == "fuck yourself")
+{
+    label1.Text = "Code Valid";
+
+    label6.Text = "Would if I could " + (theUsername) + "." + Environment.NewLine + Environment.NewLine + "would if I could...";
+}
+
+if (theCode == "fuck me")
+{
+    label1.Text = "Code Valid";
+
+    System.Windows.Forms.MessageBox.Show(";)"); 
+}
+
+if (theCode == "list of codes")
+{
+    label1.Text = "Code Valid";
+
+    label6.Text = "Did you seriously think you'd find the list of code?" + Environment.NewLine + "Although there is a list of codes hidden somewhere, it isn't here!" + Environment.NewLine + Environment.NewLine + "...However clicking the 6G logo could be a start...";
+}
+
+if (theCode == "fuck you")
+            {
+                label1.Text = "Fuck you";
+
+                label2.Text = "Fuck you";
+
+                button1.Enabled = false;
+
+                textBox1.Enabled = false;
+
+                label6.Text = "No " + (theUsername) + "," + Environment.NewLine + "How about FUCK YOU";
+                
+
+
+            }
+            if (theCode == "help")
+            {
+                label1.Text = "Code Valid";
+
+                label6.Text = "Don't panic " + (theUsername) + "," + Environment.NewLine + "To open the guide again, type: guide :into the text box." + Environment.NewLine + "Or, for a more detailed guide, type in: full guide";
+            }
+
+            if (theCode == "g83hn0")
+            {
+
+                label6.Text = "Code not yet implimented";
+
+            }
+            else
+            {
+                
+            }
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "jwn6ma";
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            string theCodeV2 = textBox2.Text;
+
+            if (theCodeV2 == "help")
+            {
+                System.Windows.Forms.MessageBox.Show("if (theCodeV2 == 3f7yj8)" + Environment.NewLine + "{" + Environment.NewLine + "System.Windows.Forms.MessageBox.Show_protocol_t76" + Environment.NewLine + "}");
+            }
+
+            if (theCodeV2 == "3f7yj8")
+            {
+                System.Windows.Forms.MessageBox.Show("Wow! if you're seeing this you must be a GENIUS. As a reward, type:" + Environment.NewLine + Environment.NewLine + "winner" + Environment.NewLine + Environment.NewLine + "in to the text box!");
+            }
+
+            if (theCodeV2 == "winner")
+            {
+
+                button2.Enabled = false;
+
+                textBox1.Enabled = true;
+
+                button1.Text = "yuo are smart";
+
+                button2.Text = "yuo are smart";
+
+                label2.Text = "yuo are smart, yuo are smart, yuo are smart, yuo are smart";
+
+                label1.Text = "yuo are smart";
+
+                textBox1.Text = "yuo are smart";
+
+                textBox2.Text = "yuo are smart";
+
+                System.Windows.Forms.MessageBox.Show("yuo are smart");
+
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string theUsername = textBox3.Text;
+
+            System.Windows.Forms.MessageBox.Show("Welcome to the S.Box, " + (theUsername) + "!" + Environment.NewLine + "----------" + Environment.NewLine + "Hopefully using this application will provide useful and not a pain to use. Whether it is or it isn't, thats your problem now until I make a patch for your problem XD. In the mean time this short little guide will be teach you how to use the S.Box to its maximum capacity!" + Environment.NewLine + "----------" + Environment.NewLine + "How to use/enter a code" + Environment.NewLine + Environment.NewLine + "after reading this quick instruction manual all feature in the S.Box will be opened up! (if this is your second time reading this just click the exit button or the OK button at the bottem to get stuck right in). when all is unlocked you will see:" + Environment.NewLine + "*- A text box to enter your code/question" + Environment.NewLine + "*- A button to click once the code/question is written to generate an answer" + Environment.NewLine + "*- A small label under the button to tell you whether or not what you just entered triggers a response" + Environment.NewLine + "*- And a disclaimer/hint on how to enter your questions (not so much codes)" + Environment.NewLine + "----------" + Environment.NewLine + "The S.Box is not only for reading specific messages from codes, but you can also talk with the S.Box! (to a certain degree...). Or, if you look close enough/give that much of a damn. you can take on the S.Box head to head in a challenge to figure out the 10 digit, master code. That is if you can even find out how..." + Environment.NewLine + "----------" + Environment.NewLine + "How to recognise a code" + Environment.NewLine + Environment.NewLine + "there are many codes, each is 6 letter/numbers long and often a combination of both. aaaaaaaaaaand thats all you really need to know! just enter them into the box, click the button and get the intended message! enjoy! (or not XD)");
+
+            label1.Visible = true;
+
+            button1.Visible = true;
+
+            textBox1.Visible = true;
+
+            label2.Visible = true;
+
+            button3.Visible = false;
+
+            button4.Visible = false;
+
+            label3.Visible = false; 
+
+            label4.Visible = false;
+
+            label6.Visible = true;
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            
+            string theUsername = textBox3.Text;
+
+            pictureBox1.Visible = true;
+
+            label3.Visible = true;
+
+            label4.Visible = true;
+
+            button3.Visible = true;
+
+            button4.Visible = true;
+
+            button5.Visible = false;
+
+            textBox3.Visible = false;
+
+            label5.Visible = false;
+
+            label7.Visible = false;
+
+            label3.Text = "Welcome " + (theUsername) + "!";
+
+            label4.Text = "and welcome to V0.02 of the S.Box!";
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+
+            label1.Visible = true;
+
+            button1.Visible = true;
+
+            textBox1.Visible = true;
+
+            label2.Visible = true;
+
+            button3.Visible = false;
+
+            button4.Visible = false;
+
+            label3.Visible = false;
+
+            label4.Visible = false;
+
+            label6.Visible = true;
+
+        }
+    }
+}
